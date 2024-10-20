f407502e6ba069a2c53897ad4f101b1b4157d622
Luiz Filipe <luiz.neves@prosas.com.br>
2024-10-20 01:01:14 -0300

    Fix test config

--- spec/async_request_reply/config_spec.rb
+++ spec/async_request_reply/config_spec.rb
@@ -1,23 +1,8 @@
 require "minitest/autorun"
 require 'async_request_reply'
+
 describe ::AsyncRequestReply::Config do
-
-	describe 'When init with default configs' do
-		before do
-			@config = ::AsyncRequestReply::Config
-		end
-
-		it "Should init with redis as a repository" do
-			_(@config.configured.repository_adapter).must_equal AsyncRequestReply::RepositoryAdapters::RedisRepositoryAdapter
-			_(@config.configured.redis_url_conection).must_equal 'redis://localhost:6379'
-		end
-
-		it "Should init with AsyncRequestReply::WorkersEngine::Async as a async_engine" do
-			_(@config.configured.async_engine).must_equal AsyncRequestReply::WorkersEngine::Async
-		end
-	end
-
-	describe 'When change default configs' do
+	describe 'When set configs' do
 		before do
 			@config = ::AsyncRequestReply.config do |conf|
 				conf.async_engine = :sidekiq
