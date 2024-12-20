11f7d03386b89eefe9fb77e472bd19d60bf1c78b
Rafael <rafael.pinheiro@prosas.com.br>
2024-12-20 15:37:45 -0300

    uses client

--- lib/async_request_reply/repository_adapters/redis_repository_adapter.rb
+++ lib/async_request_reply/repository_adapters/redis_repository_adapter.rb
@@ -7,18 +7,20 @@ module AsyncRequestReply
 	module RepositoryAdapters
 		class RedisRepositoryAdapter < AbstractRepositoryAdapter
 			class << self
-				@@redis ||= Redis.new(url: AsyncRequestReply::Config.instance.redis_url_conection)
-
 				def get(uuid)
-					@@redis.get(uuid)
+					client.get(uuid)
 				end
 
 				def del(uuid)
-					@@redis.del(uuid)
+					client.del(uuid)
 				end
 
 				def setex(uuid, ttl, payload)
-					@@redis.setex(uuid, ttl, payload)
+					client.setex(uuid, ttl, payload)
+				end
+
+				def client
+					@@redis ||= Redis.new(url: AsyncRequestReply::Config.instance.redis_url_conection)
 				end
 			end
 		end
