8c2392dbbf4445516d5364844fa48a72e437cd14
raphael-souza <raphael3130@gmail.com>
2025-01-09 13:16:19 -0300

    fix contagem de erros no processamento do worker

--- spec/async_request_reply/worker_in_batch_spec.rb
+++ spec/async_request_reply/worker_in_batch_spec.rb
@@ -1,97 +1,112 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'async_request_reply'
 require 'byebug'
 
 describe AsyncRequestReply::WorkerInBatch do
-	# Feito pelo GPT
-	
-	class Fibonacci
-	  attr_accessor :sequence_cache
+  # Feito pelo GPT
 
-	  def initialize
-	    @sequence_cache = {}
-	  end
+  class Fibonacci
+    attr_accessor :sequence_cache
 
-	  # Método para calcular Fibonacci recursivamente
-	  def recursive(n)
-	    return n if n <= 1
-	    recursive(n - 1) + recursive(n - 2)
-	  end
+    def initialize
+      @sequence_cache = {}
+    end
 
-	  # Método para gerar a sequência até o enésimo número
-	  def sequence(up_to)
-	    (0..up_to).map { |n| @sequence_cache[n] ||= recursive(n) }
-	  end
-	end
+    # Método para calcular Fibonacci recursivamente
+    def recursive(n)
+      return n if n <= 1
 
+      recursive(n - 1) + recursive(n - 2)
+    end
 
-	let(:work1) { AsyncRequestReply::Worker.new({ class_instance: 1, methods_chain: [[:+, 1], [:*, 2]]}) }
-	let(:work2) { AsyncRequestReply::Worker.new({ class_instance: 2, methods_chain: [[:*, 2], [:+, 2]]}) }
-	let(:work3_erro) { AsyncRequestReply::Worker.new({ class_instance: 2, methods_chain: [[:*, 2], [:+, 2], [:/, 0]]}) }
-	let(:worker_in_batch) { AsyncRequestReply::WorkerInBatch.new([work1,work2]) }
+    # Método para gerar a sequência até o enésimo número
+    def sequence(up_to)
+      (0..up_to).map { |n| @sequence_cache[n] ||= recursive(n) }
+    end
+  end
 
-	describe 'meta_data' do
-		it 'should save meta_data' do
-			worker_in_batch = AsyncRequestReply::WorkerInBatch.new
-			worker_in_batch.meta = {'redirect_url': 'url'}
-			worker_in_batch.save
-			_(AsyncRequestReply::WorkerInBatch.find(worker_in_batch.id).meta["redirect_url"]).must_equal 'url'
-		end
-	end
+  let(:work1) { AsyncRequestReply::Worker.new({ class_instance: 1, methods_chain: [[:+, 1], [:*, 2]] }) }
+  let(:work2) { AsyncRequestReply::Worker.new({ class_instance: 2, methods_chain: [[:*, 2], [:+, 2]] }) }
+  let(:work3_error) { AsyncRequestReply::Worker.new({ class_instance: 2, methods_chain: [[:*, 2], [:+, 2], [:/, 0]] }) }
+  let(:worker_in_batch) { AsyncRequestReply::WorkerInBatch.new([work1, work2]) }
 
-	describe '.find' do
-		it 'should find work' do
-			worker_in_batch = AsyncRequestReply::WorkerInBatch.new
-			worker_in_batch.workers = [work1]
-			worker_in_batch.save
-			_(AsyncRequestReply::WorkerInBatch.find(worker_in_batch.id).id).must_equal worker_in_batch.id
-		end
-	end
+  describe 'meta_data' do
+    it 'should save meta_data' do
+      worker_in_batch = AsyncRequestReply::WorkerInBatch.new
+      worker_in_batch.meta = { 'redirect_url': 'url' }
+      worker_in_batch.save
+      _(AsyncRequestReply::WorkerInBatch.find(worker_in_batch.id).meta['redirect_url']).must_equal 'url'
+    end
+  end
 
-	describe '.find!' do
-		it 'should raise exception case not found' do
-			assert_raises AsyncRequestReply::WorkerInBatch::WorkerInBatchNotFound do
-				worker_in_batch = AsyncRequestReply::WorkerInBatch.new
-				worker_in_batch.workers = [work1]
-				worker_in_batch.save
-				AsyncRequestReply::WorkerInBatch.find!('not_found')
-	    end
-		end
-	end
+  describe '.find' do
+    it 'should find work' do
+      worker_in_batch = AsyncRequestReply::WorkerInBatch.new
+      worker_in_batch.workers = [work1]
+      worker_in_batch.save
+      _(AsyncRequestReply::WorkerInBatch.find(worker_in_batch.id).id).must_equal worker_in_batch.id
+    end
+  end
 
-	describe '.perform' do
-		before do
-			AsyncRequestReply.configure do |conf|
-				conf.add_message_pack_factory do |factory|
-					factory[:first_byte] = 0x09
-					factory[:klass] = Fibonacci
-					factory[:packer] = lambda { |instance, packer|
-		      	packer.write_string(instance.sequence_cache.to_json)
-		      }
-					factory[:unpacker] = lambda { |unpacker|
-		        data = unpacker.read
-		        instance = Fibonacci.new
-		        instance.sequence_cache = JSON.parse(data)
-		        instance
-		      }
-		      factory
-				end
-			end
-		end
+  describe '.find!' do
+    it 'should raise exception case not found' do
+      assert_raises AsyncRequestReply::WorkerInBatch::WorkerInBatchNotFound do
+        worker_in_batch = AsyncRequestReply::WorkerInBatch.new
+        worker_in_batch.workers = [work1]
+        worker_in_batch.save
+        AsyncRequestReply::WorkerInBatch.find!('not_found')
+      end
+    end
+  end
 
-		it 'Using with Fibonacci' do
-			worker1 = AsyncRequestReply::Worker.new({ class_instance: Fibonacci.new, methods_chain: [[:sequence, 35]]})
-			worker2 = AsyncRequestReply::Worker.new({ class_instance: Fibonacci.new, methods_chain: [[:sequence, 35]]})
-			batch = AsyncRequestReply::WorkerInBatch.new
-			batch.workers = [worker1, worker2]
-			batch.save
-			batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
-			_(batch.perform)
-			batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
-			_(batch.worker_ids.count).must_equal 2
-			_(batch.start_time).wont_be_nil
-			_(batch.end_time).wont_be_nil
-			_(batch.elapsed).wont_be_nil
-		end
-	end
+  describe '.perform' do
+    before do
+      AsyncRequestReply.configure do |conf|
+        conf.add_message_pack_factory do |factory|
+          factory[:first_byte] = 0x09
+          factory[:klass] = Fibonacci
+          factory[:packer] = lambda { |instance, packer|
+            packer.write_string(instance.sequence_cache.to_json)
+          }
+          factory[:unpacker] = lambda { |unpacker|
+            data = unpacker.read
+            instance = Fibonacci.new
+            instance.sequence_cache = JSON.parse(data)
+            instance
+          }
+          factory
+        end
+      end
+    end
+
+    it 'Using with Fibonacci' do
+      worker1 = AsyncRequestReply::Worker.new({ class_instance: Fibonacci.new, methods_chain: [[:sequence, 35]] })
+      worker2 = AsyncRequestReply::Worker.new({ class_instance: Fibonacci.new, methods_chain: [[:sequence, 35]] })
+      batch = AsyncRequestReply::WorkerInBatch.new
+      batch.workers = [worker1, worker2]
+      batch.save
+      batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
+      _(batch.perform)
+      batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
+      _(batch.worker_ids.count).must_equal 2
+      _(batch.start_time).wont_be_nil
+      _(batch.end_time).wont_be_nil
+      _(batch.elapsed).wont_be_nil
+    end
+
+    it 'Using with error' do
+      batch = AsyncRequestReply::WorkerInBatch.new
+      batch.workers = [work3_error, work1, work2]
+      batch.save
+      batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
+      _(batch.perform)
+      batch = AsyncRequestReply::WorkerInBatch.find(batch.id)
+      _(batch.worker_ids.count).must_equal 3
+      _(batch.start_time).wont_be_nil
+      _(batch.end_time).wont_be_nil
+      _(batch.elapsed).wont_be_nil
+      _(batch.successes.count).must_equal 2
+      _(batch.failures.count).must_equal 1
+    end
+  end
 end
--- spec/async_request_reply/worker_spec.rb
+++ spec/async_request_reply/worker_spec.rb
@@ -1,115 +1,132 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'async_request_reply'
 require 'byebug'
 
 describe ::AsyncRequestReply::Worker do
-	describe 'when perform with all workflow defined' do
-		before do
-			@async_request = ::AsyncRequestReply::Worker.new({
-				class_instance: 1, methods_chain: [[:+, 1], [:*, 2]],
-				success: {
-					class_instance: "self",
-					methods_chain: [[:+, 1]]
-				},
-				failure: {
-					class_instance: "self",
-					methods_chain: [[:*, 3]]
-				},
-				redirect_url: "teste"
-			})
-			
-			@async_request.save
-		end
+  describe 'when perform with all workflow defined' do
+    before do
+      @async_request = ::AsyncRequestReply::Worker.new({
+                                                         class_instance: 1, methods_chain: [[:+, 1], [:*, 2]],
+                                                         success: {
+                                                           class_instance: 'self',
+                                                           methods_chain: [[:+, 1]]
+                                                         },
+                                                         failure: {
+                                                           class_instance: 'self',
+                                                           methods_chain: [[:*, 3]]
+                                                         },
+                                                         redirect_url: 'teste'
+                                                       })
 
-		it 'perform' do
-			_(AsyncRequestReply::Worker.find(@async_request.id).perform).must_equal 5
-			_(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
-			_(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
-			_(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
-			_(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal "done"
-		end
+      @async_request.save
+    end
 
-		describe 'failure workflow' do
-			describe 'internal_server_error' do
-				it '.perform' do
-					@async_request.methods_chain = [[:/,0]]
-					@async_request.save
-					assert_nil(AsyncRequestReply::Worker.find(@async_request.id).perform)
-					_(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal "internal_server_error"
-				end
-			end
-			describe 'fail' do
-				it '.perform' do
-					@async_request.methods_chain = [[:==,2]]
-					@async_request.failure = {class_instance: 0, methods_chain: []}
-					@async_request.save
-					_(AsyncRequestReply::Worker.find(@async_request.id).perform).must_equal 0
-					_(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
-					_(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal "unprocessable_entity"
-				end
-			end
-		end
+    it 'perform' do
+      _(AsyncRequestReply::Worker.find(@async_request.id).perform).must_equal 5
+      _(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
+      _(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
+      _(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
+      _(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal 'done'
+    end
 
-		it 'destroy' do
-			@async_request.destroy(0)
-		end
+    describe 'failure workflow' do
+      describe 'internal_server_error' do
+        it '.perform' do
+          @async_request.methods_chain = [[:/, 0]]
+          @async_request.save
+          assert_nil(AsyncRequestReply::Worker.find(@async_request.id).perform)
+          _(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal 'internal_server_error'
+        end
+      end
+      describe 'fail' do
+        it '.perform' do
+          @async_request.methods_chain = [[:==, 2]]
+          @async_request.failure = { class_instance: 0, methods_chain: [] }
+          @async_request.save
+          _(AsyncRequestReply::Worker.find(@async_request.id).perform).must_equal 0
+          _(AsyncRequestReply::Worker.find(@async_request.id).start_time).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.id).end_time).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.id).elapsed).wont_be_nil
+          _(AsyncRequestReply::Worker.find(@async_request.uuid).status).must_equal 'unprocessable_entity'
+        end
+      end
+    end
 
-		it 'perform_async' do
-			@async_request.perform_async
-		end
+    it 'destroy' do
+      @async_request.destroy(0)
+    end
 
-		it 'find' do
-			AsyncRequestReply::Worker.find(@async_request.id)
-		end
-	end
+    it 'perform_async' do
+      @async_request.perform_async
+    end
 
-	describe 'when perform with some parts of workflow' do
-		before do
-			@async_request = ::AsyncRequestReply::Worker.new
-		end
+    it 'find' do
+      AsyncRequestReply::Worker.find(@async_request.id)
+    end
+  end
 
-		it 'should not perform with not have class_instance' do
-			assert_nil(@async_request.perform)
-		end
+  describe 'when perform with some parts of workflow' do
+    before do
+      @async_request = ::AsyncRequestReply::Worker.new
+    end
 
-		describe 'should perform when have class_instance' do
-			it 'with constant 1' do
-				@async_request.class_instance = 1
-				 _(@async_request.perform).must_equal 1
-			end
+    it 'should not perform with not have class_instance' do
+      assert_nil(@async_request.perform)
+    end
 
-			it 'with constant File' do
-				AsyncRequestReply.configure do |conf|
-					conf.add_message_pack_factory do |factory|
-						factory[:first_byte] = 0x0A
-						factory[:klass] = File
-						factory[:packer] = lambda { |instance, packer|
-							packer.write_string(instance.path)
-							encoded_file = File.read(instance.path)
-			      	packer.write_string(encoded_file)
-			      }
-						factory[:unpacker] = lambda { |unpacker|
-							file_name = unpacker.read
-							bytes_temp_file = unpacker.read
-							file = File.new
-							file.binmode
-							file.write(bytes_temp_file)
-							file.close
-							file
-			      }
-			      factory
-					end
-				end
-				
-				file = File.new("./file.txt", "w")
-				@async_request.class_instance = file
-				_(@async_request.perform).must_equal file
-			end
-		end
-	end
+    describe 'should perform when have class_instance' do
+      it 'with constant 1' do
+        @async_request.class_instance = 1
+        _(@async_request.perform).must_equal 1
+      end
+
+      it 'with constant File' do
+        AsyncRequestReply.configure do |conf|
+          conf.add_message_pack_factory do |factory|
+            factory[:first_byte] = 0x0A
+            factory[:klass] = File
+            factory[:packer] = lambda { |instance, packer|
+              packer.write_string(instance.path)
+              encoded_file = File.read(instance.path)
+              packer.write_string(encoded_file)
+            }
+            factory[:unpacker] = lambda { |unpacker|
+              file_name = unpacker.read
+              bytes_temp_file = unpacker.read
+
+              # Criando um Tempfile
+              tempfile = Tempfile.new('meu_arquivo_temp')
+              tempfile.write('Este é um conteúdo temporário.')
+              tempfile.rewind
+
+              # Definindo o caminho para o arquivo permanente
+              file_path = file_name
+
+              # Copiando o conteúdo do Tempfile para um arquivo permanente
+              File.open(file_path, 'w') do |file|
+                file.write(tempfile.read)
+              end
+
+              # Fechando e excluindo o Tempfile
+              tempfile.close
+              tempfile.unlink
+              # debugger
+              File.open(file_path)
+            }
+            factory
+          end
+        end
+
+        file = File.new('./file.txt', 'w')
+
+        @async_request.class_instance = file
+        _(@async_request.perform.path).must_equal file.path
+
+        _(File.open(@async_request.perform.path).read).must_equal File.open(file).read
+      end
+    end
+  end
 end
